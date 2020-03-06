804916246d5ac1c82ad75f96bee2a93fc33d11e6
noctui <61877276+noctui@users.noreply.github.com>
2020-03-06 09:42:19 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,17 @@
+$(".btn3").click(function(){
+    var Noun1 = $(".noun1").val(); 
+    console.log(Noun1); 
+    $(".noun1Val").text(Noun1);
+});
+
+$(".btn6").click(function(){
+    var Place = $(".place").val(); 
+    console.log(Place); 
+    $(".placeVal").text(Place);
+});
+
+$(".btn5").click(function(){
+    var Verb1 = $(".verb1").val(); 
+    console.log(Verb1); 
+    $(".verb1Val").text(Verb1);
+});
